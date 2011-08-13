5c61968f30ed481e340ea69216b6f551fc451e06
Conan Zhang <bsspirit@163.com>
2011-08-13 15:44:58 +0800

    'u'

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -9,15 +9,15 @@ class SiteController extends Controller
 	{
 		return array(
 			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
+//			'captcha'=>array(
+//				'class'=>'CCaptchaAction',
+//				'backColor'=>0xFFFFFF,
+//			),
 			// page action renders "static" pages stored under 'protected/views/site/pages'
 			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
+//			'page'=>array(
+//				'class'=>'CViewAction',
+//			),
 		);
 	}
 

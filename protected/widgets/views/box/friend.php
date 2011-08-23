e55b3c3c14964c1bba28193839b67b4f07cf5f8a
Conan Zhang <bsspirit@163.com>
2011-08-23 16:41:55 +0800

    'u'

--- protected/widgets/views/box/friend.php
+++ protected/widgets/views/box/friend.php
@@ -10,6 +10,6 @@
 		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/5.jpg"/></a></p>
 		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/6.jpg"/></a></p>
 		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/11.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/13.jpg"/></a></p>
+		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/12.jpg"/></a></p>
 	 </div> 
 </div>

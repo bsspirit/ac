b1cd2a2a8c94a07983cba1e12442617dbf281cbb
Conan Zhang <bsspirit@163.com>
2011-10-28 13:20:07 +0800

    a

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -10,13 +10,19 @@ $this->menu=array(
 );
 ?>
 
-<h1>北京奥诚兴业科技发展有限公司</h1>
-<br/><p>地址：北京市朝阳区南磨房路16号禧福汇R3-501</p>
-<br/><p>邮编：100021</p>
-<br/><p>Tel:010-67186799  010-67187099</p>
-<br/><p>Fax:010-67187019</p>
-<br/><p>E-mail：ac_carrier2001@sina.com</p>
-<br/><p>联系人：杨小姐</p>
-<br/><p>QQ：1139355159</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+<div class="mybox">
+	 <div class="title">
+	 	<strong class="name">联系我们</strong>
+	 </div>
+	 <div class="content">
+	 	<p class="indent">北京奥诚兴业科技发展有限公司</p>
+		<br/><p class="indent">地址：北京市朝阳区南磨房路16号禧福汇R3-501</p>
+		<br/><p class="indent">邮编：100021</p> 
+		<br/><p class="indent">Tel:010-67186799  010-67187099</p>
+		<br/><p class="indent">Fax:010-67187019<br/>
+		<br/><p class="indent">联系人：杨小姐</p>
+		<br/><p class="indent">QQ：1139355159</p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+	 </div> 
+</div>

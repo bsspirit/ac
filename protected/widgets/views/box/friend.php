ca1d3e02c66b4699b5bac6d9f437337d74dbbcf3
Conan Zhang <bsspirit@163.com>
2011-11-17 12:23:11 +0800

    'a'

--- protected/widgets/views/box/friend.php
+++ protected/widgets/views/box/friend.php
@@ -1,15 +1,31 @@
+<?php $basepath=Yii::app()->request->baseUrl;?>
+<script type="text/javascript" src="<?php echo $basepath ?>/js/friend/jcarousellite_1.0.1.pack.js"></script>
+<script type="text/javascript">
+	$(function() {
+	    $(".marquee").jCarouselLite({
+	        auto: 1000,
+    		speed: 1000,
+    		vertical: true,
+    		visible: 4
+	    });
+	});
+</script>
 <div class="mybox">
 	 <div class="title">
 	 	<strong class="name">合作伙伴</strong>
 	 </div>
 	 <div class="content">
-	 	<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/1.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/2.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/3.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/4.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/5.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/6.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/11.jpg"/></a></p>
-		<p><a href="#"><img width="180" src="<?php echo Yii::app()->request->baseUrl; ?>/upfiles/friend/12.jpg"/></a></p>
+		 <div class="marquee">
+		    <ul>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/1.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/2.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/3.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/4.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/5.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/6.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/11.jpg"/></a></li>
+		        <li><a href="#"><img width="180" src="<?php echo $basepath ?>/upfiles/friend/12.jpg"/></a></li>
+		    </ul>
+		</div>
 	 </div> 
 </div>

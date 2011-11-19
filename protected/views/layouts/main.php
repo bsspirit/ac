b92be5dbc8b25f8d0fc34e511e3b15b9179cbe6b
Conan Zhang <bsspirit@163.com>
2011-11-19 11:04:23 +0800

    'a'

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -25,6 +25,17 @@
 	<script type="text/javascript" src="<?php echo $basepath ?>/js/nav/jquery-latest.js"></script>
 	
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-22898039-3']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
 	
 </head>
 
